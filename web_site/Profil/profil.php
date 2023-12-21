14253584a339085d49b4a0a878ae4c7ffbcdb631
LimuleSempai <125760323+LimuleSempai@users.noreply.github.com>
2023-12-21 15:22:34 +0100

    profil

--- web_site/Profil/profil.php
+++ web_site/Profil/profil.php
@@ -1,4 +1,101 @@
+<?php
+session_start();
+include("../connect_db/db.php");
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $AdminID = $_POST['Id'];
+// if(isset($_SESSION['username'])) {
+//   echo "Your session is running " . $_SESSION['userName'];
+// }
+    
+
+// if (!isset($_SESSION['username'])) {
+//     header('Location: ../home/');
+//     exit();
+//     }
+// MySQL database connection
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "bibliotheque_ia";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Function to sanitize input data
+function sanitize($data) {
+    return htmlspecialchars(stripslashes(trim($data)));
+}
+
+// Check if form is submitted for updating user data
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Sanitize user input to prevent SQL injection
+    $id = sanitize($_POST["id"]);
+    $name = sanitize($_POST["name"]);
+    $email = sanitize($_POST["email"]);
+    $tel = sanitize($_POST["tel"]);
+
+    // Update user data in the database
+    $sql = "UPDATE users SET name='$name', email='$email', tel='$tel' WHERE AdminId=$id";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Record updated successfully";
+    } else {
+        echo "Error updating record: " . $conn->error;
+    }
+}
+
+// Fetch user data from the database
+$id = session_id(); // Assuming the user is logged in with ID 1 (you should implement proper authentication)
+$sql = "SELECT * FROM users WHERE id=$id";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $name = $row["name"];
+    $email = $row["email"];
+    $tel = $row["tel"];
+} else {
+    echo "User not found";
+}
+
+$conn->close();
+?>
+
 <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>User Profile</title>
+</head>
+<body>
+
+    <div class="profile-container">
+        <h2>User Profile</h2>
+        <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+            <input type="hidden" name="id" value="<?php echo $id; ?>">
+            <label for="name">Name:</label>
+            <input type="text" name="name" value="<?php echo $name; ?>" required>
+
+            <label for="email">Email:</label>
+            <input type="email" name="email" value="<?php echo $email; ?>" required>
+
+            <label for="tel">Tel:</label>
+            <input type="tel" name="tel" value="<?php echo $tel; ?>" required>
+
+            <input type="submit" value="Update Profile">
+        </form>
+    </div>
+
+</body>
+</html>
+
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -9,7 +106,7 @@
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<!------ Include the above in your HEAD tag ---------->
+---- Include the above in your HEAD tag -------
 
 <div class="container emp-profile">
             <form method="post">
@@ -79,4 +176,6 @@
         </div>
     
 </body>
-</html>
+</html> -->
+
+
