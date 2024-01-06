9d53d62b928a795276823b473705aa0c87ba9d33
Todin <112660117+Todin13@users.noreply.github.com>
2024-01-06 23:42:05 +0100

    nav bar

--- web_site/administration/administration_data.php
+++ web_site/administration/administration_data.php
@@ -75,6 +75,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="administration_data.css">
     <title>Administrations des données</title>
 </head>
@@ -151,6 +152,30 @@
         };
 
     </script>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="../home/home.php">Home</a>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="../home/browse.html">Search</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../administration/administration_data.php">Administration</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../tableau_de_bord/tableau_de_bord.php">Dashboard</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../Profil/profil.php">Profil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../logout/logout.php">Lougout</a>
+                </li>
+            
+        </div>
+    </nav>
 
     <div class="container">
         <div class="left-column">
@@ -229,5 +254,10 @@
             ?>
         </div>
     </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
 </body>
 </html>
